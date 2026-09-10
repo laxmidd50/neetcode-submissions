ea7941d08b74018c8a13185ef0b85becc0c5e4c5
laxmidd50 <laxmidd50@gmail.com>
2026-09-10 16:22:02 -0400

    Add: dynamicArray - submission-2

---
+++ Data Structures & Algorithms/dynamicArray/submission-2.cpp
@@ -0,0 +1,50 @@
+#include <cstring>
+
+class DynamicArray {
+public:
+
+    DynamicArray(int capacity) {
+        m_data = std::make_unique_for_overwrite<int[]>(capacity);
+        m_capacity = capacity;
+        m_size = 0;
+    }
+
+    int get(int i) {
+        return m_data[i];
+    }
+
+    void set(int i, int n) {
+        m_data[i] = n;
+    }
+
+    void pushback(int n) {
+        if (m_size == m_capacity)
+        {
+            resize();
+        }
+        m_data[m_size++] = n;
+    }
+
+    int popback() {
+        return m_data[--m_size];
+    }
+
+    void resize() {
+        std::unique_ptr<int[]> temp_arr = std::make_unique_for_overwrite<int[]>(m_capacity*2);
+        std::memcpy(temp_arr.get(), m_data.get(), m_capacity*sizeof(int));
+        m_capacity = m_capacity*2;
+        m_data = std::move(temp_arr);
+    }
+
+    int getSize() {
+        return m_size;
+    }
+
+    int getCapacity() {
+        return m_capacity;
+    }
+private:
+    std::unique_ptr<int[]> m_data;
+    int m_size;
+    int m_capacity;
+};
