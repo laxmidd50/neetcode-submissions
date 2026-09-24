52536e8d6bb66bcf8da0c9b6322ede913c1fe912
laxmidd50 <laxmidd50@gmail.com>
2026-09-24 16:55:17 -0400

    Add: multiply-strings - submission-2

---
+++ Data Structures & Algorithms/multiply-strings/submission-2.cpp
@@ -0,0 +1,107 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string result = "";
+        if (isLeftBigger(num1, num2))
+            multiply(num1, num2, result);
+        else
+            multiply(num2, num1, result);
+        return result;
+    }
+private:
+    int m_base = 10;
+
+    void multiply(string & num1, string & num2, string & result)
+    {
+        for (int i = num2.size()-1; i >= 0; i--)
+        {
+            int x = toInt(num2[i]);
+            int carry = 0;
+            string res1 = "";
+            for (int j = num1.size()-1; j >= 0 || carry > 0; j--)
+            {
+                int y = getDigitInt(num1, j);
+                y = (y * x) + carry;
+                carry = y / m_base;
+                y = y % m_base;
+                res1 = toDigit(y) + res1;
+            }
+            result = add(res1, result);
+            num1 = num1 + "0";
+        }
+    }
+
+    string add(const string & num1, const string & num2)
+    {
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry = 0;
+        string result;
+        while (i >= 0 || j >= 0 || carry > 0)
+        {
+            int x = getDigitInt(num1, i--);
+            int y = getDigitInt(num2, j--);
+            int z = y + x;
+            carry = z / m_base;
+            z = z % m_base;
+            result = toDigit(z) + result;
+        }
+        return result;
+    }
+
+    bool isLeftBigger(const string & left, const string & right)
+    {
+        if (left.size() > right.size())
+            return true;
+        else if (right.size() > left.size())
+            return false;
+        else
+        {
+            for (int i = 0; i < left.size(); i++)
+            {
+                char l = left[i];
+                char r = right[i];
+                if (l > r)
+                    return true;
+                else if (r > l)
+                    return false;
+            }
+            return false;
+        }
+    }
+
+    int getDigitInt(const string & str, int index)
+    {
+        if (index < 0 || index >= str.size())
+            return 0;
+
+        return toInt(str[index]);
+    }
+
+    int toInt(const char c)
+    {
+        return (c - '0');
+    }
+
+    string toString(int x)
+    {
+        if (x == 0)
+            return "0";
+        
+        string result = "";
+        while (x > 0)
+        {
+            int y = x % m_base;
+            x = x/10;
+            result = toDigit(y) + result;
+        }
+        return result;
+    }
+
+    char toDigit(int x)
+    {
+        if (x > m_base-1)
+            throw;
+        return '0'+x;
+    }
+};
